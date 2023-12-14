0a25edbce7b8ca9433d8f210d3e7a6ec10eecfcf
Bronislav <shevchenko-bg@yandex.ru>
2023-12-14 21:01:56 +0300

    added seminar 5 file

---
+++ Seminar5.py
@@ -0,0 +1,165 @@
+# Последовательностью Фибоначчи называется
+# последовательность чисел a0 , a1, ..., an, ...,
+# где a0  = 0, a1  = 1,
+# ak = ak-1 + ak-2 (k > 1).
+# Требуется найти N-е число Фибоначчи
+
+# def fibonachchi(n):
+#     if n in [1, 2]:
+#         return 1
+#     return fibonachchi(n - 1) + fibonachchi(n - 2)
+# n = int(input())
+# print(fibonachchi(n))
+
+#--------------------------------------Задача31-----------------------------------------------
+
+# Хакер Василий получил доступ к классному журналу и
+# хочет заменить все свои минимальные оценки на
+# максимальные. Напишите программу, которая
+# заменяет оценки Василия, но наоборот: все
+# максимальные – на минимальные.
+# Input: 5 -> 1 3 3 3 4
+# Output: 1 3 3 3 1
+
+
+# from random import randint
+# import time
+
+# n = int(input('Введите количество оценок: '))
+
+# list_1 = [randint(1, 100000000) for _ in range(n)]
+# #print(list_1)
+# #print()
+# start_1 = time.time()
+# max_bal = max(list_1)
+# min_bal = min(list_1)
+
+# for i in range(len(list_1)):
+#     if max_bal == list_1[i]:
+#          list_1[i] = min_bal
+# end_1 = time.time()
+# #print(list_1)
+
+# print(f'{end_1 - start_1}')
+# #№2
+# start_2 = time.time()
+
+# list_1 = [randint(1, 100000000) for _ in range(n)]
+# max_bal = list_1[0]
+# min_bal = list_1[0]
+# #list_max_point = []
+
+# for el in set(list_1):
+#     if max_bal < el:
+#          max_bal = el
+#     if el < min_bal:
+#         min_bal = el
+# #print(max_bal, min_bal)
+
+# for i in range(len(list_1)):
+#     if max_bal == list_1[i]:
+#          list_1[i] = min_bal
+# end_2 = time.time()
+# #print(list_1)
+# print(f'{end_2 - start_2}')
+
+# #№3
+
+# start_3 = time.time()
+
+# list_1 = [randint(1, 100000000) for _ in range(n)]
+# max_bal = list_1[0]
+# min_bal = list_1[0]
+# list_max_point = [0]
+
+# for i in range(len(list_1)):
+#     if max_bal < list_1[i]:
+#          max_bal = list_1[i]
+#          list_max_point = [i]
+#     elif list_1[i] == max_bal:
+#          list_max_point.append(i)
+
+#     if list_1[i] < min_bal:
+#         min_bal = list_1[i]
+# #print(max_bal, min_bal)
+
+# for i in list_max_point:
+#     list_1[i] = min_bal
+    
+# end_3 = time.time()
+# #print(list_1)
+# print(f'{end_3 - start_3}')
+
+#-------------------------------------Задача35-------------------------------
+
+# Напишите функцию, которая принимает одно число и
+# проверяет, является ли оно простым
+# Напоминание: Простое число - это число, которое
+# имеет 2 делителя: 1 и n(само число)
+# Input: 5
+# Output: yes 
+
+
+# def prime_num(num):
+#     for div in range(2, num):
+#         if num % div == 0:
+#             return False
+#     return True
+
+# num = int(input('Введите одно число: '))
+
+# if prime_num(num) == True:
+#     print('YES')
+# else:
+#     print('NO')
+    
+ # №2   
+
+# def prime_num(num):
+#     if num != 2 and num % 2 == 0:
+#         return False
+#     for div in range(3,int(num ** 0.5) + 1, 2):
+#         if num % div == 0: 
+#             return False
+#     return True
+
+# #№3        
+        
+# num=int(input("Введите число: "))
+# print(prime_num(num)) 
+    
+# def prime_num(num):
+#     if num != 2 and num % 2 == 0:
+#         return False
+#     for div in range(3,int(num ** 0,5) + 1, 2):
+#         if num % div == 0:
+#             return False
+#     return True
+
+# num = int(input('Введите одно число: '))
+
+# if prime_num(num) == True:
+#     print('YES')
+# else:
+#     print('NO')
+
+#----------------------------------------Задача 37-------------------------
+
+# Дано натуральное число N и
+# последовательность из N элементов.
+# Требуется вывести эту последовательность в
+# обратном порядке.
+# Примечание. В программе запрещается
+# объявлять массивы и использовать циклы
+# (даже для ввода и вывода).
+# Input: 2 -> 3 4
+# Output: 4 3
+
+def reverse_nums (n):
+    if n == 1:
+        return input('Введите число последовательности: ')
+    k = input('Введите число последовательности: ')
+    return f'{reverse_nums(n - 1)} {k}'
+    
+num = int(input('Введиче количестиво чисел: '))
+print(reverse_nums(num))
